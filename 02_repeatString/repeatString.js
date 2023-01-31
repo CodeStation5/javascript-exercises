f9e2e7b4c1721b82cdc7bc0c5c80c3107c3608e3
CodeStation5 <js21.xda@gmail.com>
2023-01-30 20:59:26 -0800

    complete assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(holdHey, amount) {
+    let word = '';
+    if (amount < 0) 
+        return 'ERROR';
+    for (i = 0; i < amount; i++)
+        word = word + holdHey;
+    return word;
 };
 
 // Do not edit below this line
